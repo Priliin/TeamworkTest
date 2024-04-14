0e4769bc73898103ff9753ea6ce0c02cd956ebdb
Indrek Tonson <indrek.tonson@gmail.com>
2024-04-14 11:45:50 +0300

    Created project files

---
+++ src/main/java/com/example/TeamworkTest/TeamworkTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.TeamworkTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamworkTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamworkTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TeamworkTest/TeamworkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TeamworkTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamworkTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
